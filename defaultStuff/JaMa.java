faf073751dc97fbc3e1cdc527bf1ba799d8c4524
ButchersTower <ButchersTwoer@gmail.com>
2014-10-17 23:50:25 -0400

    trig update

--- defaultStuff/JaMa.java
+++ defaultStuff/JaMa.java
@@ -1,6 +1,124 @@
 package CodeRoots.defaultStuff;
 
 public class JaMa {
+
+	/**
+	 * Theta math
+	 */
+
+	static float pointToThea(float[] point) {
+		float pointThea = (float) Math.atan(point[1] / point[0]);
+		// sayVect("point", point);
+		// System.out.println("pointFirst: " + pointThea + " ("
+		// + (pointThea * (180 / Math.PI) + ")"));
+		if (point[1] > 0 && pointThea < 0) {
+			// System.out.println("change1");
+			pointThea = (float) Math.PI + pointThea;
+		} else if (point[1] < 0 && pointThea > 0) {
+			// y is less than zero and thea is greater than zero.
+			// System.out.println("change2");
+			pointThea = -(float) Math.PI + pointThea;
+		}
+		// System.out.println("pointMid: " + pointThea + " ("
+		// + (pointThea * (180 / Math.PI) + ")"));
+		if (pointThea == 0 && point[0] < 0) {
+			// System.out.println("zero to 360.");
+			pointThea = (float) Math.PI;
+		}
+		return pointThea;
+	}
+
+	static float[] theaToPoint(float thea, float radius) {
+		return new float[] { (float) Math.cos(thea) * radius,
+				(float) Math.sin(thea) * radius };
+	}
+
+	static float[] rotPoint(float thea, float[] point) {
+		// System.out.println("**rotPoint**");
+		// System.out.println("inThea: " + thea + " (" + ((thea) * (180 /
+		// Math.PI)));
+		float pointa = Vect2d.norm(point);
+		boolean bothNeg = false;
+		if (point[0] < 0 && point[1] < 0) {
+			// bothNeg = true;
+		}
+		// converts point to thea.
+		float pointThea = pointToThea(point);
+		// sayVect("point", point);
+		// System.out.println("pointThea: " + pointThea);
+
+		// adds theatas.
+		float newThea = pointThea + thea;
+		// System.out.println("old THEA: " + newThea + " ("
+		// + (newThea * (180 / Math.PI)) + ")");
+		if (newThea > Math.PI) {
+			newThea = newThea - (float) Math.PI * 2;
+		} else if (newThea < -Math.PI) {
+			newThea = (float) (2 * Math.PI) + newThea;
+		}
+
+		// thea to point.
+		// newPoint is hypotnuse.
+		float[] newPoint = new float[2];
+		// float nx = (float) Math.cos(newThea) * pointa;
+		// float ny = (float) Math.tan(newThea) * nx;
+		// System.out.println("sin: " + Math.sin(newThea) + "    pointa: "
+		// + pointa);
+		// System.out.println("tan: " + Math.tan(newThea));
+		// System.out.println("NEW THEA: " + newThea + " ("
+		// + (newThea * (180 / Math.PI)) + ")");
+		/**
+		 * CHEATE. radius should be pointa, but in order to make the program
+		 * lookbetter it is set to radius. Fix the code and make it so pinta is
+		 * equal to radius anyway.
+		 */
+		// System.out.println("newThea: " + newThea + "(" + newThea + ")");
+		float ny = (float) Math.sin(newThea) * pointa;
+		float nx = 1 / ((float) Math.tan(newThea) / ny);
+		if (newThea > Math.PI / 2) {
+			// System.out.println("bug ++");
+		} else if (newThea < -Math.PI / 2) {
+			// System.out.println("bug --");
+		}
+		if (Float.isNaN(nx)) {
+			nx = pointa;
+		}
+		// System.out.println("bothNeg: " + bothNeg);
+		if (bothNeg) {
+			nx = -nx;
+			ny = -ny;
+		}
+		// System.out.println("afterRot (" + nx + ", " + ny + ")");
+
+		return new float[] { nx, ny };
+	}
+
+	static float theaAdd(float thea1, float thea2) {
+		// adds two theas between -180 and 180 (in radians) and returned a thea
+		// between -180 and 180
+		float tempThea = thea1 + thea2;
+		if (tempThea > Math.PI) {
+			tempThea = -(float) Math.PI * 2 + tempThea;
+		} else if (tempThea < -Math.PI) {
+			tempThea = (float) Math.PI * 2 + tempThea;
+		}
+		return tempThea;
+	}
+
+	static float theaSub(float thea1, float thea2) {
+		// System.out.println("thea1: " + thea1 + "   thea2: " + thea2);
+		// adds two theas between -180 and 180 (in radians) and returned a thea
+		// between -180 and 180
+		float tempThea = thea1 - thea2;
+		if (tempThea > Math.PI) {
+			tempThea = -(float) Math.PI * 2 + tempThea;
+		} else if (tempThea < -Math.PI) {
+			tempThea = (float) Math.PI * 2 + tempThea;
+		}
+		// System.out.println("tempThea: " + tempThea);
+		return tempThea;
+	}
+
 	// Jacob Math
 
 	static int[] appendIntAr(int[] st, int appendage) {
